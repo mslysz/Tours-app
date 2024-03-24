c5f5f795f35997950dce4e8ecc65c1a8a0003244
mslysz <macslysz@gmail.com>
2024-03-24 04:59:33 +0000

    I have taken the fetchData function outside and called it in the useEffect

--- src/App.jsx
+++ src/App.jsx
@@ -5,29 +5,38 @@ import { Tours } from './Tours';
 const url = 'https://course-api.com/react-tours-project';
 
 const App = () => {
-  const [tours, setTour] = useState(null);
-  const [loading, setLoading] = useState(true);
-  console.log(tours);
+  const [isloading, setIsLoading] = useState(true);
+  const [tours, setTour] = useState([]);
 
+  const fetchData = async () => {
+    setIsLoading(true);
+    try {
+      const response = await fetch(url);
+      const toursData = await response.json();
+      setTour(toursData);
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+      setIsLoading(false);
+    }
+  };
   useEffect(() => {
-    const fetchData = async () => {
-      try {
-        const response = await fetch(url);
-        const tours = await response.json();
-        setTour(tours);
-        setLoading(false);
-      } catch (error) {
-        console.log(error);
-        setLoading(false);
-      }
-    };
     fetchData();
   }, []);
 
   return (
     <main>
       <h1>our tours</h1>
-      {loading ? <Loading /> : <Tours tours={tours} />}
+      {isloading ? (
+        <Loading />
+      ) : (
+        <>
+          <Tours tours={tours} />
+          <button onClick={fetchData} className='btn'>
+            Render again
+          </button>
+        </>
+      )}
     </main>
   );
 };

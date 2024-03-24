411d01139a1261a1d50b4efdd27b2e941367e0cc
mslysz <macslysz@gmail.com>
2024-03-24 04:16:05 +0000

    I have implemented the change of state when the delete button is pressed and an object in the list is deleted

--- src/Tours.jsx
+++ src/Tours.jsx
@@ -1,11 +1,15 @@
+import { useState } from 'react';
 import { Tour } from './Tour';
 
 export const Tours = ({ tours }) => {
+  const [toursList, setToursList] = useState(tours);
+  const removeTour = (id) => {
+    setToursList(toursList.filter((tour) => tour.id !== id));
+  };
   return (
     <ul className='tours'>
-      {tours.map((tour) => {
-        const { id, image, info, name, price } = tour;
-        return <Tour key={id} tour={tour} />;
+      {toursList.map((tour) => {
+        return <Tour key={tour.id} tour={tour} removeTour={removeTour} />;
       })}
     </ul>
   );
